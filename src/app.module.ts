2b30d15543dad04c93fe15ff3ab54264934a06a4
camilozv21 <camilozv21@gmail.com>
2024-03-27 07:39:45 -0500

    initial config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { EnvConfiguration } from './config/app.config';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+
+    ConfigModule.forRoot({
+      load: [ EnvConfiguration ],
+    }),
+
+    MongooseModule.forRoot(process.env.MONGODB, {
+      dbName: 'pokemonsdb'
+    }),
+
+    UsersModule,
+
+
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
